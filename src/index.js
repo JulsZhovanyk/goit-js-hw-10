b84f0963dc195ef6b68057456e857e054ad412a3
JulsZhovanyk <julia.zhovanyk@gmail.com>
2023-01-10 17:49:41 +0200

    add task code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+const inputRef = document.querySelector('input#search-box');
+const listRef = document.querySelector('.country-list');
+const infoRef = document.querySelector('.country-info');
+
+inputRef.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+function onInputSearch(evt) {
+  const inputVal = evt.target.value.trim();
+
+  deleteRenderMarkUp();
+
+  if (inputVal) {
+    fetchCountries(inputVal)
+      .then(countries => {
+        if (countries.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+          return;
+        }
+        if (countries.length >= 2) {
+          renderCountryList(countries);
+        }
+        if (countries.length === 1) {
+          renderCountryInfo(countries);
+        }
+      })
+      .catch(error => {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      });
+  }
+}
+
+function deleteRenderMarkUp() {
+  listRef.innerHTML = '';
+  infoRef.innerHTML = '';
+}
+
+function renderCountryList(countries) {
+  const countriesMarkUp = countries
+    .map(({ flags: { svg }, name: { common } }) => {
+      return `<li><img src="${svg}" width=40><h2>${common}</h2></li>`;
+    })
+    .join('');
+  listRef.innerHTML = countriesMarkUp;
+}
+
+function renderCountryInfo(countries) {
+  const countryMarkUp = countries
+    .map(
+      ({
+        flags: { svg },
+        name: { official },
+        capital,
+        population,
+        languages,
+      }) => {
+        return `<img src="${svg}" width=150><h2>${official}</h2><p><b>Capital:</b> ${
+          capital[0]
+        }</><p><b>Population:</b> ${population}</p><p><b>Languages:</b> ${Object.values(
+          languages
+        )}</p>`;
+      }
+    )
+    .join('');
+  infoRef.innerHTML = countryMarkUp;
+}
